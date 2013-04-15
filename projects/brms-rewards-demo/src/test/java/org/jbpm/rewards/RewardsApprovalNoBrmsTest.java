f060d8b29df7d1385932bac6489888cafa2419da
Eric D. Schabell <eric@schabell.org>
2013-04-15 20:39:58 +0200

    Added broken unit test when trying to interact with ht and add data.

--- projects/brms-rewards-demo/src/test/java/org/jbpm/rewards/RewardsApprovalNoBrmsTest.java
+++ projects/brms-rewards-demo/src/test/java/org/jbpm/rewards/RewardsApprovalNoBrmsTest.java
@@ -2,14 +2,26 @@ package org.jbpm.rewards;
 
 
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
 import org.drools.KnowledgeBase;
 import org.drools.builder.ResourceType;
 import org.drools.runtime.StatefulKnowledgeSession;
 import org.drools.runtime.process.ProcessInstance;
 import org.jbpm.process.instance.impl.demo.SystemOutWorkItemHandler;
+import org.jbpm.task.AccessType;
+import org.jbpm.task.Status;
+import org.jbpm.task.query.TaskSummary;
+import org.jbpm.task.service.ContentData;
+import org.jbpm.task.service.TaskService;
+import org.jbpm.task.service.responsehandlers.BlockingTaskSummaryResponseHandler;
 import org.jbpm.test.JbpmJUnitTestCase;
 import org.junit.After;
 import org.junit.Before;
@@ -24,6 +36,10 @@ import org.junit.Test;
 
 public class RewardsApprovalNoBrmsTest extends JbpmJUnitTestCase {
 
+	private static StatefulKnowledgeSession ksession;
+	private static TaskService taskService;
+	private static Map<String, Object> params;
+	private static ProcessInstance processInstance;
 
 	public RewardsApprovalNoBrmsTest() {
 
@@ -45,55 +61,92 @@ public class RewardsApprovalNoBrmsTest extends JbpmJUnitTestCase {
 	@Override
 	@After
 	public void tearDown() throws Exception {
-
-	  //ksession.dispose();
 		super.tearDown();
-
 	}
 
-	@Test
-	public void submitEmployee() {
-
-	  // load up the knowledge base
+	private void setupTestCase() {		
+		
+		// load up the knowledge base
 		KnowledgeBase kbase = null;
+	
+		// Use the local files.
+	    final Map<String, ResourceType> resources = new HashMap<String, ResourceType>();
+	    resources.put("rewardsapproval.bpmn2", ResourceType.BPMN2);
 
-    // Use the local files.
-    final Map<String, ResourceType> resources = new HashMap<String, ResourceType>();
-    resources.put("rewardsapproval.bpmn2", ResourceType.BPMN2);
+	    try {
+	    	kbase = createKnowledgeBase(resources);
+	 	} catch (Exception e) {
+	 		e.printStackTrace();
+	 	}
 
-    try {
- 			kbase = createKnowledgeBase(resources);
- 		} catch (Exception e) {
- 			e.printStackTrace();
- 	  }
-
- 	  StatefulKnowledgeSession ksession = createKnowledgeSession(kbase);
+	    ksession = createKnowledgeSession(kbase);
 		getTaskService(ksession);
 
 		// register work items.
 		ksession.getWorkItemManager().registerWorkItemHandler("Log", new SystemOutWorkItemHandler());
 		ksession.getWorkItemManager().registerWorkItemHandler("Email", new SystemOutWorkItemHandler());
 
-		// setup our input request for processing.
-		final Map<String, Object> params = new HashMap<String, Object>();
+		params = new HashMap<String, Object>();
+		// initialize process parameters.
 		params.put("employee", "erics");
-		params.put("reason", "Amazing demos for JBoss World");
-		System.out.println("=================================================");
-		System.out.println("=      Unit Test: Submit Employee Test Case        =");
-		System.out.println("=================================================");
-		
-		// start a new process instance        
-		final ProcessInstance processInstance = ksession.startProcess("org.jbpm.approval.rewards", params);
+		params.put("reason", "Amazing demos for JBoss World!");		
+	}
 
-		// Check whether the process instance has completed successfully.
- 	  	assertProcessInstanceActive(processInstance.getId(), ksession);
- 	  	System.out.println("Process is active.");
- 	  	assertNodeExists(processInstance, "Approve Reward");
- 	  	System.out.println("Process had node we are looking for.");
- 	  	assertNodeTriggered(processInstance.getId(), "Start", "Approve Reward");
- 	  	System.out.println("Process has triggerd node we expect it to do.");
- 	  	
- 	  	System.out.println("Finished test."); 	  	
+	
+	
+	@Test
+	public void rewardApprovedTest() {
+		setupTestCase();
+
+		// start process.
+		processInstance = ksession.startProcess("org.jbpm.approval.rewards", params);
+
+		// execute task by Mary from HR.		
+		List<TaskSummary> list = taskService.createSession().getTasksAssignedAsPotentialOwner("mary", new ArrayList<String>(), "en-UK");  // NP error here.
+		TaskSummary task = list.get(0);
+		taskService.createSession().setTaskStatus(task.getId(), Status.Reserved);
+		taskService.createSession().setTaskStatus(task.getId(), Status.InProgress);
+		
+		Map<String, Object> taskParams = new HashMap<String, Object>();
+		taskParams.put("Explanation", "Great work");
+		taskParams.put("Outcome", "Approved");
+		
+		// Serialized and inserted.
+		ContentData content = new ContentData();
+		content.setAccessType(AccessType.Inline);
+		content.setContent(getByteArrayFromObject(taskParams));
+		
+		// add results of task.
+		taskService.createSession().setTaskStatus(task.getId(), Status.Completed);
+		
+
+		// test for completion and in correct end node.
+		assertProcessInstanceCompleted(processInstance.getId(), ksession);
+		assertNodeTriggered(processInstance.getId(), "End Approved");
+	}
+
+	/**
+	 * Converts an object to a serialized byte array.
+	 * 
+	 * @param obj Object to be converted.
+	 * @return byte[] Serialized array representing the object.
+	 */
+	public static byte[] getByteArrayFromObject(Object obj) {
+	    byte[] result = null;
+	       
+	    try {
+	        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+	        ObjectOutputStream oos = new ObjectOutputStream(baos);
+	        oos.writeObject(obj);
+	        oos.flush();
+	        oos.close();
+	        baos.close();
+	        result = baos.toByteArray();
+	    } catch (IOException ioEx) {
+	        Logger.getLogger("UtilityMethods").error("Error converting object to byteArray", ioEx);
+	    }
+	        
+	    return result;
 	}
 
 }
